1080ce204c134a5183fcbb707d6399d949716fde
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-07-14 11:35:02 -0400

    updated the info displayed on exhibit page

--- rufus_museum/src/components/Exhibit.js
+++ rufus_museum/src/components/Exhibit.js
@@ -3,16 +3,21 @@ import {Link} from 'react-router-dom';
 class Exhibit extends React.Component{
 
   render() {
+    //save prop location state so we dont have to keep typing that out. This is the artifact that the user clicked on from previous page
+    let artifact = this.props.location.state.artifact;
     // console.log(this.props);
     return <div>
     Exhibit page
     <Link to={{
-      pathname:`/${this.props.location.state.artifact.theme}`
+      pathname:`/${artifact.theme}`
     }}>
     <button>Back</button>
     </Link>
-    <h1>{this.props.location.state.artifact.name}</h1>
-    <p>{this.props.location.state.artifact.description}</p>
+    <h1>{artifact.name}</h1>
+    <img src={artifact.image} alt={artifact.name} />
+    <p>{artifact.creator}</p>
+    <p>{artifact.year}</p>
+    <p>{artifact.description}</p>
     </div>
   }
 }
