01739399372ed68abc19f173a756c11200dd3444
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-07-18 15:39:12 -0400

    styling the home page nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,30 @@ import { Link } from 'react-router-dom';
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <p>This is the home page</p>
+      <div className="homeNavigation">
+        <h4>This is the home page</h4>
         <ul>
-          <li><Link to="/featured">Featured Exhibit</Link></li>
-          <li><Link to="/exhibits">Exhibitions</Link></li>
-          <li><Link to="/events">Events</Link></li>
+          <li>
+            <div id="featuredNavImg" className="homeNavContainer">
+              <Link to="/featured">
+                <p className="img-text">Featured Exhibit</p>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <div id="ourExhibits" className="homeNavContainer">
+              <Link to="/exhibits">
+                <p className="img-text">Our Exhibitions</p>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <div id="currentEvents" className="homeNavContainer">
+              <Link to="/events">
+              <p className="img-text">Current Events</p>
+              </Link>
+            </div>
+          </li>
         </ul>
       </div>
     )
