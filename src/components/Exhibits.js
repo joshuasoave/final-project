e06a3fba80d596ab6b895f5465d700dc475fc395
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-07-18 17:41:44 -0400

    finished styling for the exhibits nav page

--- src/components/Exhibits.js
+++ src/components/Exhibits.js
@@ -5,40 +5,36 @@ import {Link} from 'react-router-dom';
 class Exhibits extends React.Component{
   render() {
     return (
-      <div className="exhibits">
+      <div className="exhibitsNavPage">
         <h3>Exhibitions</h3>
-        <ul className="allExhibits">
+        <ul>
           <li>
-            <div className="exhibitNavContainer">
-              <Link to="/featured">
-                <img src="https://i.imgur.com/SO1CULX.jpg" alt="Image of Leap frog that takes user to featured exhibit when clicked"/>
-                <p>Featured Exhibit</p>
-              </Link>
-            </div>
+            <Link to="/featured">
+              <div className="exhibitNavImg" id="featuredExhibitsPageImg">
+                <p className="img-text">Zimbabwe Sculpture</p>
+              </div>
+            </Link>
           </li>
           <li>
-            <div className="exhibitNavContainer">
-              <Link to="/egypt">
-                <img src="https://i.imgur.com/VBtGgr9.jpg" alt="picture of the Egyptian artifact that when clicked will take the user to Egypt exhibit"/>
-                <p>Ancient Egypt</p>
-              </Link>
-            </div>
+            <Link to="/egypt">
+              <div className="exhibitNavImg" id="egyptExhibitsPageImg">
+                <p className="img-text">Ancient Egyptian Artifacts</p>
+              </div>
+            </Link>
           </li>
           <li>
-            <div className="exhibitNavContainer">
-              <Link to="/surrealism">
-                <img src="https://i.imgur.com/2IwffvO.jpg" alt="picture from the surrealism exhibit that takes a user to the surrealism exhibit when clicked"/>
-                <p>Surrealism</p>
-              </Link>
-            </div>
+            <Link to="/surrealism">
+              <div className="exhibitNavImg" id="surrealismExhibitsPageImg">
+                <p className="img-text">Surrealism: Philosophy in Art</p>
+              </div>
+            </Link>
           </li>
           <li>
-            <div className="exhibitNavContainer">
-              <Link to="/space">
-                <img src="https://i.imgur.com/rp8jzSx.jpg" alt="picture from the space exhibit that takes the user to space exhibit when clicked"/>
-                <p>Space</p>
-              </Link>
-            </div>
+            <Link to="/space">
+              <div className="exhibitNavImg" id="spaceExhibitsPageImg">
+                <p className="img-text">Visit our Planetarium</p>
+              </div>
+            </Link>
           </li>
         </ul>
       </div>
